6b74b07cbf5f301b7190ce0b375e7c6e70242d3b
DevKurskiev <devkurskiev@gmail.com>
2025-04-18 02:37:42 +0300

    add project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl">SYMBIONT 👁 Awakening…</h1>
+    </main>
+  );
+}
